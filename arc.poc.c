b6e2e4cc4f5d200e7dec501434b41bf22ae105ff
mihirbaviskar <36090347+mihirbaviskar@users.noreply.github.com>
2018-10-31 17:26:28 -0700

    Update arc.poc.c

--- arc.poc.c
+++ arc.poc.c
@@ -1,9 +1,12 @@
+
+    void turn_degrees(double degrees){
     double sum = average();
-    while(sum<(77000)*2){
+    while(sum<(((75000)*1.05)/90)*degrees){
         sum+=gyro_z();
+        mrp(0,600,9);
+        mrp(1,800,15);
         printf("%lf\n",sum);
-        mrp(0,600,6);
-        mrp(1,800,10);
-        //19.75in radius
-        //77000 is 90
-    }
+        //19.75in
+    } 
+
+
