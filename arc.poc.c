716c650718ec714b134f35b5159715dfffeb30f9
mihirbaviskar <36090347+mihirbaviskar@users.noreply.github.com>
2018-11-05 20:01:32 -0800

    Update arc.poc.c

--- arc.poc.c
+++ arc.poc.c
@@ -1,5 +1,21 @@
 
-    void turn_degrees(double degrees){
+
+
+#include<stdio.h>
+#include<math.h>
+
+
+double speed_left_wheel(double radius);
+double get_ratio(double radius);
+
+
+int main() {
+    double r = 10.8;
+    printf(" Left: %lf\n Right %lf\n",speed_left_wheel(r), (speed_left_wheel(r)*get_ratio(r)));
+    printf("Ratio %lf", get_ratio(r));
+}
+
+void turn_degrees(double degrees){
     double sum = average();
     while(sum<(((75000)*1.05)/90)*degrees){
         sum+=gyro_z();
@@ -8,5 +24,11 @@
         printf("%lf\n",sum);
         //19.75in
     } 
-
-
+double speed_left_wheel(double radius){
+    return (radius-3.153)/0.02511;
+    
+}
+double get_ratio(double radius){
+    return sqrt(24.8499/(radius-2.35752));
+    
+}
