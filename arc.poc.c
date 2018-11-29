b4d4c46d87b3f2d9f3bd240808523feae9a2de4c
mihirbaviskar <36090347+mihirbaviskar@users.noreply.github.com>
2018-11-29 15:34:23 -0800

    Update arc.poc.c

--- arc.poc.c
+++ arc.poc.c
@@ -61,6 +61,7 @@ void turn_degrees_arc(double degrees , double radius, int direction){
     double real_gyro= 0;
     double counter = 0;
     double last_gyro = 0;
+    
     if(direction == 0){
         while(sum <= ((75000*1.12)/90)*degrees){
             mrp(0,speed_wheel2(radius),9);//speed_wheel(radius)*(1/LIMITER),9);
@@ -71,6 +72,66 @@ void turn_degrees_arc(double degrees , double radius, int direction){
             counter++;
             real_gyro = gyro_z()-bias;
             sum+=real_gyro;
+            //19.75in radius for left:600 & right:800
+        }
+    }
+    else{
+        while(sum <= (((75000*1.05)/90)*degrees)){
+            mrp(0,SPEEDC,15);
+            mrp(1,speed_wheel2(radius)*LIMITER,9);
+            //printf("%lf\n",sum);
+            real_gyro = abs(gyro_z()-bias);
+            sum+=real_gyro;
+            //19.75in radius for left:600 & right:800
+        }
+        
+    }   
+		printf("Bias:%lf\n",bias);
+        printf("Average Gyro Turn%lf\n",(sum/counter));
+        printf("\t GMPC:%i\n",(gmpc(RIGHT)));
+
+}
+
+
+
+/* ------------------
+(sum / 75000*1.12)
+0  ———————————————— 1
+sum=0    sum1/75000*1.12
+rmax = 6
+rmin = 3
+diff = 3
+radius = rmax
+radius = rmax - ( sum1/75000*1.12 ) * diff
+
+radius = rmax - ( sum1/75000*1.12 ) * 3
+
+--------------------- */
+
+
+
+void turn_degrees_arc_ellipse(double degrees , double rmax, double rmin, int direction){
+    cmpc(RIGHT);
+    printf("%lf\n",degrees);
+//    double sum = 2*average();
+    double bias = average(); 
+    double sum = 0;
+    double real_gyro= 0;
+    double counter = 0;
+    double last_gyro = 0;
+    double radius = rmax;
+    double diff = rmax - rmin;  
+    if(direction == 0){
+        while(sum <= ((75000*1.12)/90)*degrees){
+            mrp(0,speed_wheel2(radius),9);//speed_wheel(radius)*(1/LIMITER),9);
+            mrp(1,800,15);
+            //printf("%lf\n",sum);
+            //printf("%lf\n",sum-last_gyro);
+            last_gyro = sum;
+            counter++;
+            real_gyro = gyro_z()-bias;
+            sum+=real_gyro;
+            radius = rmax - diff*(sum/(((75000*1.12)/90)*degrees));
             //19.75in radius for left:600 & right:800
         }
     }
